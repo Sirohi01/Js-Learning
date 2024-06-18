b5f2ca8d550a9b7c4e4f928fe60cfee15e8432e6
Sirohi01 <126563915+Sirohi01@users.noreply.github.com>
2024-06-18 06:25:10 +0000

    variables

---
+++ basic_01/01_variables.js
@@ -0,0 +1,11 @@
+const account_Id=14453
+let account_Email = "officialmanishsirohi.01@gmail.com"
+var account_Password = "123456789"
+var account_city = "Gurugram"
+
+//account_Id =23456 //not allowed
+account_Email = "manishsirohi9211@gmail.com"
+account_Password = "212121"
+account_city = "Delhi"
+
+console.table([account_Email,account_Id,account_Password,account_city])
