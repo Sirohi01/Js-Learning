068c6ceb4a8b59b45c2eec6b22b2891833324fdc
Sirohi01 <126563915+Sirohi01@users.noreply.github.com>
2024-06-18 07:02:10 +0000

    data types

---
+++ basic_01/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"//treat all js code as a newer version
+//alert("hello")//we are using nodejs not browser
+let name = "Manish"
+let age = 24
+let isLoggedIn = false
+
+// number = 2 to power 54
+//bigint
+//boolean = true/false
+//string=""
+//Null = stand alone value /  representation of empty value
+//undefined = when value is not defined 
+//symbol = unique
+
+//object
+
+console.log(typeof age)
